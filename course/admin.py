227aab5b614a9a542d4c7e9d25bc6db2268b820f
mslatour <sanderlatour@gmail.com>
2014-03-17 19:58:34 +0100

    Added lessons between course and item. Fixed #1

--- course/admin.py
+++ course/admin.py
@@ -3,7 +3,7 @@ from django.forms.widgets import NumberInput
 from course.models import *
 
 class CourseContentsInline(admin.TabularInline):
-    model = Course.items.through
+    model = Course.lessons.through
 
     def formfield_for_dbfield(self, db_field, **kwargs):
         if db_field.name == "index":
@@ -14,11 +14,23 @@ class CourseContentsInline(admin.TabularInline):
 class CourseAdmin(admin.ModelAdmin):
     inlines = [ CourseContentsInline ]
 
+class LessonContentsInline(admin.TabularInline):
+    model = Lesson.items.through
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == "index":
+            kwargs['widget'] = NumberInput()
+        return super(LessonContentsInline, self).formfield_for_dbfield(
+                db_field, **kwargs)
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [ LessonContentsInline ]
 
 class ExternalResourceAdmin(admin.ModelAdmin):
     exclude = ('body', 'related')
 
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Resource)
 admin.site.register(ExternalResource, ExternalResourceAdmin)
 admin.site.register(Test)
