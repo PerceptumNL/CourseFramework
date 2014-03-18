c96e94947971554708e349bd36b15337fa528904
mslatour <sanderlatour@gmail.com>
2014-03-18 03:01:49 +0100

    Cleanup up admin file for course

--- course/admin.py
+++ course/admin.py
@@ -6,23 +6,11 @@ from adminsortable.admin import SortableInlineAdminMixin
 class CourseContentsInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Course.lessons.through
 
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == "index":
-            kwargs['widget'] = NumberInput()
-        return super(CourseContentsInline, self).formfield_for_dbfield(
-                db_field, **kwargs)
-
 class CourseAdmin(admin.ModelAdmin):
     inlines = [ CourseContentsInline ]
 
 class LessonContentsInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Lesson.items.through
-
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        if db_field.name == "index":
-            kwargs['widget'] = NumberInput()
-        return super(LessonContentsInline, self).formfield_for_dbfield(
-                db_field, **kwargs)
 
 class LessonAdmin(admin.ModelAdmin):
     inlines = [ LessonContentsInline ]
@@ -34,7 +22,6 @@ class ResourceAdmin(admin.ModelAdmin):
 
 class ExternalResourceAdmin(admin.ModelAdmin):
     exclude = ('body', 'related')
-
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
