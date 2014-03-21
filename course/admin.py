b6cd6c0cf41dbde7349a80a6e410f76e9d818097
mslatour <sanderlatour@gmail.com>
2014-03-21 01:23:39 +0100

    Added verbose names and reordered models in admin.py

--- course/admin.py
+++ course/admin.py
@@ -5,45 +5,9 @@ from adminsortable.admin import SortableInlineAdminMixin
 from django_summernote.admin import SummernoteModelAdmin
 from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin
 
-class ResourceAdmin(PolymorphicChildModelAdmin, SummernoteModelAdmin):
-    base_model = Resource
-    def get_queryset(self, request):
-        qs = super(ResourceAdmin, self).get_queryset(request)
-        return qs.exclude(resource_type=Resource.TYPE_EXTERNAL)
-
-class ExternalResourceAdmin(PolymorphicChildModelAdmin):
-    base_model = ExternalResource
-
-class TestContentsInline(SortableInlineAdminMixin, admin.TabularInline):
-    model = Test.questions.through
-    extra = 1
-    verbose_name = 'Test question'
-    verbose_name_plural = 'Test questions'
-
-class TestAdmin(PolymorphicChildModelAdmin):
-    base_model = Test
-    inlines = [TestContentsInline]
-
-class CourseContentsInline(SortableInlineAdminMixin, admin.TabularInline):
-    model = Course.lessons.through
-
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [ CourseContentsInline ]
-
-class ResourceInline(admin.StackedInline):
-    model = Resource
-
-class TestInline(admin.StackedInline):
-    model = Test
-
-class ItemAdmin(PolymorphicParentModelAdmin):
-    base_model = Item
-    child_models = (
-        (Resource, ResourceAdmin),
-        (ExternalResource, ExternalResourceAdmin),
-        (Test, TestAdmin),
-    )
-
+############
+# Question #
+############
 class QuestionOptionForm(forms.ModelForm):
     label = forms.CharField(label='Label:')
     value = forms.CharField(label='Answer:')
@@ -65,7 +29,6 @@ class MCOptionsInline(admin.TabularInline):
     verbose_name = "Choice"
     verbose_name_plural = "Choices"
 
-
 class MultipleChoiceQuestionAdmin(PolymorphicChildModelAdmin):
     base_model = MultipleChoiceQuestion
     exclude = ('options',)
@@ -78,14 +41,68 @@ class QuestionParentAdmin(PolymorphicParentModelAdmin):
         (MultipleChoiceQuestion, MultipleChoiceQuestionAdmin),
     )
 
+########
+# Test #
+########
+class TestContentsInline(SortableInlineAdminMixin, admin.TabularInline):
+    model = Test.questions.through
+    extra = 1
+    verbose_name = 'Test question'
+    verbose_name_plural = 'Test questions'
+
+class TestInline(admin.StackedInline):
+    model = Test
+
+class TestAdmin(PolymorphicChildModelAdmin):
+    base_model = Test
+    inlines = [TestContentsInline]
+
+############
+# Resource #
+############
+class ResourceInline(admin.StackedInline):
+    model = Resource
+
+class ResourceAdmin(PolymorphicChildModelAdmin, SummernoteModelAdmin):
+    base_model = Resource
+    def get_queryset(self, request):
+        qs = super(ResourceAdmin, self).get_queryset(request)
+        return qs.exclude(resource_type=Resource.TYPE_EXTERNAL)
+
+class ExternalResourceAdmin(PolymorphicChildModelAdmin):
+    base_model = ExternalResource
+
+
+########
+# Item #
+########
+class ItemAdmin(PolymorphicParentModelAdmin):
+    base_model = Item
+    child_models = (
+        (Resource, ResourceAdmin),
+        (ExternalResource, ExternalResourceAdmin),
+        (Test, TestAdmin),
+    )
+
+##########
+# Lesson #
+##########
 class LessonContentsInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Lesson.items.through
 
 class LessonAdmin(admin.ModelAdmin):
     inlines = [ LessonContentsInline ]
 
+##########
+# Course #
+##########
+class CourseContentsInline(SortableInlineAdminMixin, admin.TabularInline):
+    model = Course.lessons.through
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [ CourseContentsInline ]
+
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
-admin.site.register(ExternalResource, ExternalResourceAdmin)
 admin.site.register(Item, ItemAdmin)
 admin.site.register(Question, QuestionParentAdmin)
